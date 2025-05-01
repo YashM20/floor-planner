c8c550bc81530d3535b792392fb01aa02ba6dfe1
Yash <67644300+YashM20@users.noreply.github.com>
2025-05-01 13:47:08 +0530

    feat: 3js demo

---
+++ src/components/canvas/View.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
+import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
+import { Three } from '@/helpers/three/Three'
+
+export const Common = ({ color }: { color: string }) => (
+  <Suspense fallback={null}>
+    {color && <color attach='background' args={[color]} />}
+    <ambientLight />
+    <pointLight position={[20, 30, 10]} intensity={3} decay={0.2} />
+    <pointLight position={[-10, -10, -10]} color='blue' decay={0.2} />
+    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+  </Suspense>
+)
+
+const View = forwardRef(({ children, orbit, className, ...props }: { children?: React.ReactNode, orbit?: boolean, className?: string, props?: any }, ref) => {
+  const localRef = useRef(null)
+  useImperativeHandle(ref, () => localRef.current)
+
+  return (
+    <>
+      <div ref={localRef} {...props} className={className} />
+      <Three>
+        <ViewImpl track={localRef as any}>
+          {children}
+          {orbit && <OrbitControls />}
+        </ViewImpl>
+      </Three>
+    </>
+  )
+})
+View.displayName = 'View'
+
+export { View }
